aa10303a7655a95d4d3c0fd09358de0d48a29d16
ReadyPlayerSpork <lukeaaronmiller@gmail.com>
2026-03-24 22:07:02 -0600

    fixed a database migration issue

--- backend/ChatKingsApp/ChatKingsApp/Program.cs
+++ backend/ChatKingsApp/ChatKingsApp/Program.cs
@@ -53,6 +53,12 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ChatKingsDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
